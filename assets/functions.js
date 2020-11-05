f99a083cea16f49b4c0ab0464aa0cc4c5dee2b6f
mochi-dot-py <65631838+mochi-dot-py@users.noreply.github.com>
2020-11-04 22:33:51 -0500

    Added managers column to data being displayed when viewing employees.

--- assets/functions.js
+++ assets/functions.js
@@ -6,9 +6,8 @@ async function viewEmployees() {
   connection = await mysql.createConnection(mysqlConnection);
   // Displays employee data from different tables
   const [employeeData] = await connection.query(
-    "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary FROM employee INNER JOIN role ON (employee.role_id = role.id) INNER JOIN department ON (department.id = role.department_id)"
+    "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role ON (employee.role_id = role.id) LEFT JOIN department ON (department.id = role.department_id) LEFT JOIN employee manager ON (manager.id = employee.manager_id);"
   );
-  // CONCAT(employee.first_name, ' ', employee.last_name) AS manager
   console.table(employeeData);
 }
 
